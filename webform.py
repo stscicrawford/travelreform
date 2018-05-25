aaaf42760fd3ae97e942b8f381d5362235636d3b
Miles Currie <mhc13@my.fsu.edu>
2018-05-25 11:16:17 -0400

    changes?

--- webform.py
+++ webform.py
@@ -16,7 +16,7 @@ class ReusableForm(Form):
 def hello():
     form = ReusableForm(request.form)
  
-    print form.errors
+    print(form.errors)
     if request.method == 'POST':
         name=request.form['name']
         print name
