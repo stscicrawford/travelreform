c0665a840edb18a43071e4127c8a883e3722d4f4
Keira Brooks <kbrooks@stsci.edu>
2018-05-25 15:15:50 -0400

    Created transportation and rental car and hotel sections

--- transportation.py
+++ transportation.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField, DateField
 
 
 # App config.
@@ -9,32 +9,22 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class TransportationForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    departure = TextField('Departure City:', validators=[validators.required()])
-    arrival = TextField('Arrival City:', validators=[validators.required()])
-    date_depart = TextField('Date of departure:', validators=[validators.required()])
-    date_return = TextField('Date of return:', validators=[validators.required()])
+    departure = TextField('Departure City:', validators=[validators.InputRequired()])
+    arrival = TextField('Arrival City:', validators=[validators.InputRequired()])
+    date_depart = DateField('Date of departure (mm/dd/yyyy):', format='%m/%d/%y',
+                            validators=[validators.InputRequired()])
+    date_return = DateField('Date of return (mm/dd/yyyy):', format='%m/%d/%y',
+                            validators=[validators.InputRequired()])
 
-    rental = SelectField('Is a rental car needed?', choices = [('yes','yes'), ('no', 'no')], validators = [validators.required()])
-    hotel = SelectField('Is a hotel needed?', choices = [('yes','yes'), ('no', 'no')], validators = [validators.required()])
+    rental = SelectField('Is a rental car needed?',
+                         choices=[('no_rental', 'No'), ('yes_rental','Yes')],
+                         validators=[validators.InputRequired()])
+    hotel = SelectField('Is a hotel needed?',
+                        choices=[('no_hotel', 'No'), ('yes_hotel','Yes')],
+                        validators=[validators.InputRequired()])
+    other = TextField('Other (special requests):')
+    comments = TextAreaField('Comments:', [validators.Length(max=500)])
 
-
-# @app.route("/", methods=['GET', 'POST'])
-# def hello():
-#     form = ReusableForm(request.form)
-#
-#     print(form.errors)
-#     if request.method == 'POST':
-#         name=request.form['name']
-#         print(name)
-#
-#         if form.validate():
-#             # Save the comment here.
-#             flash('Hello ' + name)
-#         else:
-#             flash('All the form fields are required. ')
-#
-#     return render_template('webform.html', form=form)
 
 @app.route("/", methods=['GET', 'POST'])
 def transportation():
@@ -43,7 +33,6 @@ def transportation():
     print(form.errors)
     if request.method == 'POST':
         #General Transportation
-        name = request.form['name']
         departure = request.form['departure']
         arrival = request.form['arrival']
         date_depart = request.form['date_depart']
@@ -52,15 +41,15 @@ def transportation():
         # Rental Car and Hotel
         rental = request.form['rental']
         hotel = request.form['hotel']
-        # print(name)
-        # print(departure)
-        # print(arrival)
-        # print(date_depart)
-        # print(date_return)
+
+        other = request.form['other']
+        comments = request.form['comments']
+
+        print(departure)
 
         if form.validate():
             # Save the comment here.
-            flash('Hello ' + name)
+            flash('Hello ')
         else:
             flash('All the form fields are required. ')
 
